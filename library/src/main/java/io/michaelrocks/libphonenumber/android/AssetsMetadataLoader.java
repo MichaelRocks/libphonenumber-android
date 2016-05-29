315da8f8b298c168af69c51939a4ad53c122f0c8
Michael Rozumyanskiy <michael.rozumyanskiy@gmail.com>
2016-05-29 16:16:57 +0300

    Add a metadata loader that loads data from assets

---
+++ library/src/main/java/io/michaelrocks/libphonenumber/android/AssetsMetadataLoader.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2016 Michael Rozumyanskiy
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.michaelrocks.libphonenumber.android;
+
+import android.content.res.AssetManager;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class AssetsMetadataLoader implements MetadataLoader {
+  private final AssetManager assetManager;
+
+  public AssetsMetadataLoader(final AssetManager assetManager) {
+    this.assetManager = assetManager;
+  }
+
+  @Override
+  public InputStream loadMetadata(final String metadataFileName) {
+    String assetFileName = metadataFileName.substring(1);
+    try {
+      return assetManager.open(assetFileName);
+    } catch (final IOException exception) {
+      return null;
+    }
+  }
+}
