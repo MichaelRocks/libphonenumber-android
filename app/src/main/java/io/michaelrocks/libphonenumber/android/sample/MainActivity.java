b154664e76625e54bd382b381c0262af9e3f5556
Michael Rozumyanskiy <michael.rozumyanskiy@gmail.com>
2016-05-29 16:45:57 +0300

    Add a sample application for the library

---
+++ app/src/main/java/io/michaelrocks/libphonenumber/android/sample/MainActivity.java
@@ -0,0 +1,38 @@
+package io.michaelrocks.libphonenumber.android.sample;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+import io.michaelrocks.libphonenumber.android.NumberParseException;
+import io.michaelrocks.libphonenumber.android.PhoneNumberUtil;
+import io.michaelrocks.libphonenumber.android.Phonenumber;
+
+public class MainActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+
+    final TextView textView = (TextView) findViewById(R.id.textView);
+    findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+      private PhoneNumberUtil util = null;
+
+      @Override
+      public void onClick(final View v) {
+        if (util == null) {
+          util = PhoneNumberUtil.createInstance(getApplicationContext());
+        }
+
+        try {
+          final Phonenumber.PhoneNumber phoneNumber = util.parse("8005551212", "US");
+          textView.setText(util.format(phoneNumber, PhoneNumberUtil.PhoneNumberFormat.INTERNATIONAL));
+        } catch (NumberParseException e) {
+          e.printStackTrace();
+        }
+      }
+    });
+  }
+}
