fd2136198c8df0c4d60b6b41d7b4725bdc4cdf82
Michael Rozumyanskiy <michael.rozumyanskiy@gmail.com>
2016-05-29 15:26:39 +0300

    Implement a metadata loader that loads data from resources

---
+++ library/src/main/java/io/michaelrocks/libphonenumber/android/ResourceMetadataLoader.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2016 Michael Rozumyanskiy
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.michaelrocks.libphonenumber.android;
+
+import java.io.InputStream;
+
+class ResourceMetadataLoader implements MetadataLoader {
+  private final Class<?> loaderClass;
+
+  public ResourceMetadataLoader() {
+    this(ResourceMetadataLoader.class);
+  }
+
+  public ResourceMetadataLoader(Class<?> loaderClass) {
+    this.loaderClass = loaderClass;
+  }
+
+  @Override
+  public InputStream loadMetadata(String metadataFileName) {
+    return loaderClass.getResourceAsStream(metadataFileName);
+  }
+}
